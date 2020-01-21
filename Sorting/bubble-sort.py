e4f16c1129716d7031e3b928e251ab21403d3899
Your Name <you@example.com>
2020-01-21 10:01:28 +0100

    fist commit

---
+++ Sorting/bubble-sort.py
@@ -0,0 +1,38 @@
+#Creo la lista
+
+
+
+
+#...
+
+
+
+
+
+#Calcolo i tempi del sort
+#....
+
+
+
+
+
+
+#calcolo i tempi del bubble
+#....
+
+
+
+
+#calcolo i tempi del sort di defaut
+#....
+
+
+
+
+
+
+
+
+
+#Salvo i risultati
+#...
